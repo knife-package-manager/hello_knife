250c76fc560ff4a10793ee6abc87c73030bb5baa
17do <fucho.021000@gmail.com>
2024-08-20 10:42:30 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+# I will write the steps to build here.
+# Makefile can also be used instead.
+# For more information, see https://github.com/17do/hello_knife_makefile
+
+gcc main.c -o hello_knife
+
